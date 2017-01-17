862090eee51be53ecb3acda1e9324aeaeaca30fb
sylvia villegas <sylvia@svillegas.com>
2017-01-17 14:36:52 -0500

    Perform ste 1 (the second one)

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,18 @@
 # create grocery_list array
 grocery_list = ["chocolate", "apples", "peanut butter", "cheese"]
 
-# present each item on a new line following a *
+# present each item on a new line following a (*)
 grocery_list.each do |item|
   puts "* #{item} \n"
+end
+
+# add rice to list
+grocery_list << "rice"
+# output list again
+grocery_list.each do |item|
+  puts "* #{item} \n"
+end
+# create method to output list
+def print_list(array)
+  array.each { |item| puts "* #{item} \n" }
 end
