4b011115ffc4442787251b8455d779831862d133
sylvia villegas <sylvia@svillegas.com>
2017-01-17 14:29:17 -0500

    Performs step 1

---
+++ exercise6.rb
@@ -0,0 +1,7 @@
+# create grocery_list array
+grocery_list = ["chocolate", "apples", "peanut butter", "cheese"]
+
+# present each item on a new line following a *
+grocery_list.each do |item|
+  puts "* #{item} \n"
+end
