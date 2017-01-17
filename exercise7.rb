de83b230e962f9abeb8f02c2ecf9e91120a952fb
sylvia villegas <sylvia@svillegas.com>
2017-01-17 15:18:29 -0500

    Perform step 2

--- exercise7.rb
+++ exercise7.rb
@@ -4,3 +4,13 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+
+# Create method to display lsit of k:v pairs
+def print_list(all_students)
+  all_students.each do |cohort, count|
+    puts "#{cohort}: #{count} students"
+  end
+end
+
+print_list(students)
