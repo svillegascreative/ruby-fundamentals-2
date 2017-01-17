7153cb5ad46c2d8b0697106ec9ec69414909c480
sylvia villegas <sylvia@svillegas.com>
2017-01-17 14:47:33 -0500

    Perform step 3

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,12 @@
-# create grocery_list array
+# Create grocery_list array
 grocery_list = ["chocolate", "apples", "peanut butter", "cheese"]
 
-# present each item on a new line following a (*)
+# Present each item on a new line following a (*)
 grocery_list.each do |item|
   puts "* #{item} \n"
 end
 
-# add rice to list
+# Add rice to list
 grocery_list << "rice"
 # output list again
 grocery_list.each do |item|
@@ -17,5 +17,12 @@ def print_list(array)
   array.each { |item| puts "* #{item} \n" }
 end
 
-# how many things?
+# hHw many things?
 puts "#{grocery_list.count} items on the list"
+
+# Are bananas on the list?
+if grocery_list.include?("bananas") == false
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
